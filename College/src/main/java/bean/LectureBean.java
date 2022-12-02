64286bbf0d2576dd4cb1d7fd52b9c72f25f97ce9
leesanghung3 <leesanghung3@gmail.com>
2022-12-02 13:30:59 +0900

    2022-11-08

--- College/src/main/java/bean/LectureBean.java
+++ College/src/main/java/bean/LectureBean.java
@@ -6,36 +6,45 @@ public class LectureBean {
 	private String lecCredit;
 	private String lecTime;
 	private String lecClass;
+
 	public String getLecNO() {
 		return lecNO;
 	}
+
 	public void setLecNO(String lecNO) {
 		this.lecNO = lecNO;
 	}
+
 	public String getLecName() {
 		return lecName;
 	}
+
 	public void setLecName(String lecName) {
 		this.lecName = lecName;
 	}
+
 	public String getLecCredit() {
 		return lecCredit;
 	}
+
 	public void setLecCredit(String lecCredit) {
 		this.lecCredit = lecCredit;
 	}
+
 	public String getLecTime() {
 		return lecTime;
 	}
+
 	public void setLecTime(String lecTime) {
 		this.lecTime = lecTime;
 	}
+
 	public String getLecClass() {
 		return lecClass;
 	}
+
 	public void setLecClass(String lecClass) {
 		this.lecClass = lecClass;
 	}
-	
-	
+
 }
--- College/src/main/java/db/DBCP.java
+++ College/src/main/java/db/DBCP.java
@@ -9,9 +9,9 @@ import javax.sql.DataSource;
 
 public class DBCP {
 	private static DataSource ds = null;
-	
+
 	public static Connection getConnection() throws NamingException, SQLException {
-		if(ds == null) {
+		if (ds == null) {
 			ds = (DataSource) new InitialContext().lookup("java:comp/env/dbcp_java1_college");
 		}
 		return ds.getConnection();
